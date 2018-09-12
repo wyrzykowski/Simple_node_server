4217bc65ef91321c2360c331746fa33dc3a7942a
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2018-09-12 23:14:42 +0200

    list file

--- main.js
+++ main.js
@@ -12,21 +12,12 @@ fs.access(path, fs.constants.F_OK, (err) => {
      
  
  
-if (process.argv.length <= 2) {
-    console.log("Usage: " + __filename + " path/to/directory");
-    process.exit(-1);
-}
- 
-var path = process.argv[2];
- 
-fs.readdir(path, function(err, items) {
-    console.log(items);
- 
-    for (var i=0; i<items.length; i++) {
-        console.log(items[i]);
-    }
-});
-
+    
+     const fs = require('fs');
+     
+     fs.readdirSync('/').forEach(file => {
+       console.log(file);
+     })
     //end of read file
 
  fs.readFile(path, function(err, data) {
