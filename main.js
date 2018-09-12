dbdb1044a2c2a3b73cf31b1d7172f458792f6695
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2018-09-13 00:03:26 +0200

    modules

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
 // HTTP node's Server
 var http = require('http');
+var dt = require('./testModule.js');
 var fs = require('fs');
 var path = 'index.html';
 http.createServer(function (req, res) {
 
-if(req.url!='/' &&  req.url!='') { // test tutaj
-path = req.url;
-path=path.substr(1, path.length);} // substr to cut "/" from path string
+if(req.url!='/' &&  req.url!='') { //reading path 
+    path = req.url;
+    path=path.substr(1, path.length);// substr to cut "/" from path string
+} 
 
 fs.access(path, fs.constants.F_OK, (err) => {
- if(!err){ 
- fs.readFile(path, function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end()
-    });
-}
-else{ // if error 
-    path='404.html';
-    fs.readFile(path, function(err, data) {
+    if(!err){ 
+    console.log(myDateTime());
+
+     fs.readFile(path, function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
-    
-    
         res.end()
-    });
-}
-path='index.html';//to refresh path othervise will be still other than home page
+        });
+    }
+    else{ // if error 
+        path='404.html';
+        fs.readFile(path, function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end()
+        });
+    }
+    path='index.html';//to refresh path othervise will be still other than home page
 
-});
-}).listen(80);
+    });
+}).listen(80); // listening on 80 port
 
