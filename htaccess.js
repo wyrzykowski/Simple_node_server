62bebce5119e475ef41edec732a35e3944aba364
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2018-09-13 14:51:52 +0200

    super

--- htaccess.js
+++ htaccess.js
@@ -10,7 +10,7 @@ exports.getLink = function (pathF) {
             var strData=data.toString('utf8');
             if(strData.lastIndexOf("RewriteEngine on")!=-1)
             {
-                console.log(' Rewrite EngineOn'+pathF);
+                //console.log(' Rewrite EngineOn');
                 var pos;
                 if((pos=strData.lastIndexOf("RewriteRule "+pathF))!=-1)
                 {
