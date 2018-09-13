88fff627894d3142cead91a36e5c76a494468c86
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2018-09-14 01:39:29 +0200

    access problem sync

--- main.js
+++ main.js
@@ -11,13 +11,18 @@ if(req.url!='/' &&  req.url!='') { //reading path
     path=path.substr(1, path.length);// substr to cut "/" from path string
 } 
 
-
+const promise = new Promise(function(resolve,reject){
     path=htaccessMod.getLink(path);
-    
+    console.log("zwrocilem:"+path);
+    if(path!=undefined)
+    {
+         console.log("zwrocilem:"+path);
+    resolve(htaccessMod.getLink(path));
+    }
    
+});
 
-
-
+promise.then(function(path){
 console.log("in main path is:"+path);
 fs.access(path, fs.constants.F_OK, (err) => {
     if(!err){ 
@@ -39,6 +44,6 @@ fs.access(path, fs.constants.F_OK, (err) => {
     path='index.html';//to refresh path othervise will be still other than home page
 
     });
-
+});
 }).listen(80); // listening on 80 port
 
